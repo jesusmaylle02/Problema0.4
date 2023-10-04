89f891410e22d76e95c51702281265ffc42dca82
USER <USER@DESKTOP-KCU37G9>
2023-10-04 15:21:41 -0500

    Agregar archivos de proyecto.

---
+++ Problema0.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema0._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número : ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numero; i++)
+            {
+                for (int j = 0; j < numero; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
